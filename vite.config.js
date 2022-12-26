547d75b54414e6a5adb7f228a95f66d3aff63522
nullchinchilla <nullchinchilla@pm.me>
2022-12-26 16:52:58 -0600

    legacy support test

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import ViteYaml from '@modyfi/vite-plugin-yaml';
+import legacy from '@vitejs/plugin-legacy'
+
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [ViteYaml(), sveltekit()]
+	plugins: [ViteYaml(), sveltekit(), legacy({
+		targets: ['defaults', 'IE 11'],
+	  })]
 };
 
 export default config;
