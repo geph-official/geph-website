505c1452a563f5e0a43702df4db284e8635be8b8
nullchinchilla <nullchinchilla@pm.me>
2022-12-26 17:04:11 -0600

    Revert "legacy support test"
    This reverts commit 547d75b54414e6a5adb7f228a95f66d3aff63522.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import ViteYaml from '@modyfi/vite-plugin-yaml';
-import legacy from '@vitejs/plugin-legacy'
-
 
 /** @type {import('vite').UserConfig} */
 const config = {
